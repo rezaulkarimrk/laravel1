12a02b8b9c8aa5ac711ac16947448e21cd5ade39
Rezaul Karim RK <mrk99rk@gmail.com>
2023-05-09 11:54:17 -0400

    update

---
+++ website/lesson5/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label for="">Enter a number to count down from: </label>
+        <input type="text" name="counter">
+        <input type="submit" value="start">
+    </form>
+</body>
+</html>
+
+echo
+
+<?php
+?>
