762bc2cb2ab5eaebc2df642de4491b3b2876bda7
Rezaul Karim RK <mrk99rk@gmail.com>
2023-05-10 03:30:56 -0400

    updatae

--- website/lesson13/locations.php
+++ website/lesson13/locations.php
@@ -7,11 +7,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Location</title>
 </head>
 <body>
     </br>
-    This is homepage </br>
+    This is Location Page </br>
     Stuff about your home page can go here </br>
 </body>
 </html>
---
+++ website/lesson14/index.php
@@ -0,0 +1,16 @@
+<?php
+  setcookie('fav_food', 'pizza', time() + (86400 * 2), '/');
+  setcookie('fav_drink', 'coffe', time() + (86400 * 3), '/');
+  setcookie('fav_dessert', 'ice cream', time() + (86400 * 4), '/');
+
+  // foreach($_COOKIE as $key => $value){
+  //   echo "{$key} = {$value} </br>";
+  // }
+
+  if(isset($_COOKIE['fav_food'])){
+    echo "BUY SOME {$_COOKIE['fav_food']} !!!";
+  }
+  else{
+    echo "I don't know your favorite food";
+  }
+?>
