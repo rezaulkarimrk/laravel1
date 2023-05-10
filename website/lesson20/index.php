c03a26d76ed81a51dce21acbd06e7d48cb349e48
Rezaul Karim RK <mrk99rk@gmail.com>
2023-05-10 15:52:42 -0400

    update

--- website/lesson20/index.php
+++ website/lesson20/index.php
@@ -3,6 +3,52 @@
 // 2. PDO (PHP Data Object)
 
   include("database.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MySql</title>
+</head>
+<body>
+  <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+    <h2>Welcome to Facebook</h2>
+    username: </br>
+    <input type="text" name="username"> </br>
+    password: </br>
+    <input type="text" name="password"> </br>
+    <input type="submit" name="submit" value="Register">
+  </form>
+</body>
+</html>
+
+<?php
+  if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
+    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_SPECIAL_CHARS);
+
+    if(empty($username)){
+      echo "Please enter a username";
+    }
+    elseif(empty($password)){
+      echo "Please enter a password";
+    }
+    else{
+      $hash = password_hash($password, PASSWORD_DEFAULT);
+      $sql = "INSERT INTO users (user, password) VALUES ('$username', '$hash')";
+      
+      try{
+        mysqli_query($conn, $sql);
+        echo "You are now registered!";
+      }
+      catch(mysqli_sql_exception){
+        echo "Username already taken!";
+      }
+      
+    }
+  }
 
   mysqli_close($conn);
 ?>
